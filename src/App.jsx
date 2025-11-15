eb607aa5e3dbd91986ac74846803179568f47937
Farman Ullah <eng.farmanullah00@gmail.com>
2025-11-15 04:57:23 -0800

    Use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './components/About'
 import Resume from './components/Resume'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<About/>
-  },
-  {
-    path:"/resume",
-    element:<Resume/>
-  },
-  {
-    path:"/portfolio",
-    element:<Portfolio/>
-  },
-  {
-    path:"/contact",
-    element:<Contact/>
-  },
-], { basename: '/Farman' })
-
 const App = () => {
   return (
-    <div className='flex'>
-      <Sidebar/>
-      <RouterProvider router={router} />
-    </div>
+    <Router>
+      <div className='flex'>
+        <Sidebar/>
+        <div className='flex-1'>
+          <Routes>
+            <Route path='/' element={<About/>} />
+            <Route path='/resume' element={<Resume/>} />
+            <Route path='/portfolio' element={<Portfolio/>} />
+            <Route path='/contact' element={<Contact/>} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
